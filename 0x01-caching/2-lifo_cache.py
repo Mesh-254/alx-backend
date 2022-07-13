05841305b0749133c69bbbbb9dd350697487e4ca
mesh-254 <meshack1995@gmail.com>
2022-07-13 19:15:39 +0300

    LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+LIFO Cache module
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO cache class"""
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.cache_data_list = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            value = self.cache_data_list.pop()
+            del self.cache_data[value]
+            print(f'DISCARD: {value}')
+        
+        if key is not None and item is not None:
+            self.cache_data_list.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
